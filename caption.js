e7ee96f60cb5fe9535a8559d85cf26f504a761b1
450186 <146966554+450186@users.noreply.github.com>
2024-05-20 17:11:02 +0100

    blah blah

---
+++ caption.js
@@ -0,0 +1,14 @@
+let captions = {
+    "img-A": "No filter",
+    "img-B": "Blur",
+    "img-C": "Greyscale",
+    "img-D": "Box Shadow",
+
+}
+
+var image = document.getElementsByTagName("img");
+
+
+image.addEventListener("click", function(){
+    caption.innerHTML("")
+})
